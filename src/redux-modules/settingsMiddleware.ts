71e8d4e075ed493d4ff3d52b9bf2597fd780d010
Aarron Lee <aclee91@gmail.com>
2023-11-30 17:08:37 -0500

    initial settings middleware file

---
+++ src/redux-modules/settingsMiddleware.ts
@@ -0,0 +1,7 @@
+import { Action, Dispatch } from "redux";
+import { RootState } from "./store";
+
+export const settingsMiddleware =
+  (store: RootState) => (dispatch: Dispatch) => (action: Action<any>) => {
+    //
+  };
