b47872b2298a6eebce274f0bf698d791e301c648
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-01-13 21:07:00 -0500

    add logger for debugging

---
+++ src/redux-modules/logger.tsx
@@ -0,0 +1,22 @@
+import { getLogInfo } from "../backend/utils";
+
+export const logger = (store: any) => (next: any) => async (action: any) => {
+  const logInfo = getLogInfo();
+  await logInfo(
+    `----------------before ${action.type} ${action.payload}-------------`
+  );
+  await logInfo(store.getState());
+  await logInfo(`-----------------------`);
+
+  const result = next(action);
+
+  await logInfo(
+    `----------------after ${action.type} ${action.payload}-------------`
+  );
+  await logInfo(store.getState());
+  await logInfo(`-----------------------`);
+
+  return result;
+};
+
+export default logger;
