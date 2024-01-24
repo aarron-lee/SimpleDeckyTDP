60a231ccd13d2f574f4e56041ff8829dfa0e4341
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-01-23 23:28:10 -0500

    add migration for backwards compatibility for power governor

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import cpu_utils
 from plugin_settings import merge_tdp_profiles, get_saved_settings, get_tdp_profile, get_active_tdp_profile, set_setting as persist_setting
 from gpu_utils import get_gpu_frequency_range
 import steam_patch
+import migrations
 
 class Plugin:
 
@@ -154,19 +155,22 @@ class Plugin:
     # Migrations that should be performed before entering `_main()`.
     async def _migration(self):
         decky_plugin.logger.info("Migrating")
+
+        migrations.migrate_power_control()
+
         # Here's a migration example for logs:
         # - `~/.config/decky-template/template.log` will be migrated to `decky_plugin.DECKY_PLUGIN_LOG_DIR/template.log`
-        decky_plugin.migrate_logs(os.path.join(decky_plugin.DECKY_USER_HOME,
-                                               ".config", "decky-template", "template.log"))
+        # decky_plugin.migrate_logs(os.path.join(decky_plugin.DECKY_USER_HOME,
+        #                                        ".config", "decky-template", "template.log"))
         # Here's a migration example for settings:
         # - `~/homebrew/settings/template.json` is migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/template.json`
         # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/`
-        decky_plugin.migrate_settings(
-            os.path.join(decky_plugin.DECKY_HOME, "settings", "template.json"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".config", "decky-template"))
+        # decky_plugin.migrate_settings(
+        #     os.path.join(decky_plugin.DECKY_HOME, "settings", "template.json"),
+        #     os.path.join(decky_plugin.DECKY_USER_HOME, ".config", "decky-template"))
         # Here's a migration example for runtime data:
         # - `~/homebrew/template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
         # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
-        decky_plugin.migrate_runtime(
-            os.path.join(decky_plugin.DECKY_HOME, "template"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".local", "share", "decky-template"))
+        # decky_plugin.migrate_runtime(
+        #     os.path.join(decky_plugin.DECKY_HOME, "template"),
+        #     os.path.join(decky_plugin.DECKY_USER_HOME, ".local", "share", "decky-template"))
