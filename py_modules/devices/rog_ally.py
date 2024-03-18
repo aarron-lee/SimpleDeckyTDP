3f38b8ea7391f6231bc54cce4bd69ee4b4f2c9b3
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-03-17 20:39:19 -0400

    update platform profile code for asus

--- py_modules/devices/rog_ally.py
+++ py_modules/devices/rog_ally.py
@@ -11,74 +11,71 @@ FAST_WMI_PATH ='/sys/devices/platform/asus-nb-wmi/ppt_fppt'
 SLOW_WMI_PATH = '/sys/devices/platform/asus-nb-wmi/ppt_pl2_sppt'
 STAPM_WMI_PATH = '/sys/devices/platform/asus-nb-wmi/ppt_pl1_spl'
 
-def set_asusctl_platform_profile(tdp):
-  current_value = ''
-  if os.path.exists(PLATFORM_PROFILE_PATH):
-    with open(PLATFORM_PROFILE_PATH, 'r') as file:
-      current_value = file.read()
-      file.close()
+# def set_asusctl_platform_profile(tdp):
+#   current_value = ''
+#   if os.path.exists(PLATFORM_PROFILE_PATH):
+#     with open(PLATFORM_PROFILE_PATH, 'r') as file:
+#       current_value = file.read()
+#       file.close()
 
-  if ASUSCTL_PATH:
-    commands = [ASUSCTL_PATH, 'profile', '-P']
+#   if ASUSCTL_PATH:
+#     commands = [ASUSCTL_PATH, 'profile', '-P']
 
-    if tdp < 13:
-      commands.append('Quiet')
-    elif tdp < 20:
-      commands.append('Balanced')
-    else:
-      commands.append('Performance')
+#     if tdp < 13:
+#       commands.append('Quiet')
+#     elif tdp < 20:
+#       commands.append('Balanced')
+#     else:
+#       commands.append('Performance')
 
-    if commands[-1].lower() == current_value.strip():
-      # already set, return
-      return
+#     if commands[-1].lower() == current_value.strip():
+#       # already set, return
+#       return
 
-    results = subprocess.call(commands)
+#     results = subprocess.call(commands)
 
-    if results.stderr:
-      decky_plugin.logger.error(f"{__name__} asusctl error {results.stderr}")
+#     if results.stderr:
+#       decky_plugin.logger.error(f"{__name__} asusctl error {results.stderr}")
 
-    sleep(1.0)
+#     sleep(1.0)
 
-    return results
+#     return results
 
 def set_platform_profile(tdp):
-
-  if os.path.exists(PLATFORM_PROFILE_PATH):
-    with open(PLATFORM_PROFILE_PATH, 'r') as file:
-      current_value = file.read()
-      file.close()
-
-      command = 'quiet'
-      if tdp < 13:
-        command = 'quiet'
-      elif tdp < 20:
-        command = 'balanced'
-      else:
-        command = 'performance'
-
-      if current_value.strip() == command:
-        # already set, return
-        return
-
-      result = execute_bash_command(command, PLATFORM_PROFILE_PATH)
-      if result.stderr:
-        decky_plugin.logger.error(f"{__name__} platform_profile error {result.stderr}")
-      sleep(1.0)
-      return result
-
+  command = 'quiet'
+  if tdp < 13:
+    command = 'quiet'
+  elif tdp < 20:
+      command = 'balanced'
+  else:
+    command = 'performance'
+  try:
+    with open(PLATFORM_PROFILE_PATH, 'w') as file:
+        file.write(command)
+  except Exception as e:
+      decky_plugin.logger.error(f"{__name__} platform_profile error {result.stderr}")
+        
+  sleep(1.0)
+  return True
 
 def ryzenadj(tdp):
   try:
     # fast limit
-    file_write(tdp+2, FAST_WMI_PATH)
+    with open(FAST_WMI_PATH, 'w') as file:
+      file.write(tdp+2)
+      file.close()
     sleep(0.1)
 
     # slow limit
-    file_write(tdp, SLOW_WMI_PATH)
+    with open(SLOW_WMI_PATH, 'w') as file:
+      file.write(tdp)
+      file.close()
     sleep(0.1)
 
     # stapm limit
-    file_write(tdp, STAPM_WMI_PATH)
+    with open(STAPM_WMI_PATH, 'w') as file:
+      file.write(tdp)
+      file.close()
     sleep(0.1)
   except Exception as e:
     decky_plugin.logger.error(f"{__name__} asus wmi tdp error {e}")
@@ -92,9 +89,3 @@ def execute_bash_command(command, path):
   cmd = f"echo '{command}' | tee {path}"
   result = subprocess.run(cmd, timeout=1, shell=True, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   return result
-
-def file_write(value, path):
-  if os.path.exists(path):
-    with open(path, 'w') as file:
-      file.write(value)
-      file.close()
