073a800558352fa5fafe0effe32fe0cfb5816c94
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-04-02 13:21:10 -0400

    create App compontent without decky-frontend-lib dependency

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { FC, memo } from "react";
+import TdpRange from "./components/molecules/TdpRange";
+import { TdpSlider } from "./components/molecules/TdpSlider";
+import { PollTdp } from "./components/molecules/PollTdp";
+import { store } from "./redux-modules/store";
+import { Provider } from "react-redux";
+import { TdpProfiles } from "./components/molecules/TdpProfiles";
+import {
+  useFetchInitialStateEffect,
+  useIsInitiallyLoading,
+} from "./hooks/useInitialState";
+import Gpu from "./components/molecules/Gpu";
+import AdvancedOptions, {
+  useIsSteamPatchEnabled,
+} from "./components/molecules/AdvancedOptions";
+import OtaUpdates from "./components/molecules/OtaUpdates";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { SteamPatchDefaultTdpSlider } from "./components/molecules/SteamPatchDefaultTdpSlider";
+import PowerControl from "./components/molecules/PowerControl";
+import { DeckySection } from "./components/atoms/DeckyFrontendLib";
+
+const App: FC = memo(({}) => {
+  useFetchInitialStateEffect();
+
+  const loading = useIsInitiallyLoading();
+
+  const steamPatchEnabled = useIsSteamPatchEnabled();
+
+  return (
+    <>
+      {!loading && (
+        <>
+          <DeckySection>
+            <TdpProfiles />
+            {!steamPatchEnabled && (
+              <>
+                <TdpSlider />
+                <Gpu />
+              </>
+            )}
+          </DeckySection>
+          <PowerControl />
+          {steamPatchEnabled && <SteamPatchDefaultTdpSlider />}
+          <TdpRange />
+          <PollTdp />
+          <AdvancedOptions />
+          <ErrorBoundary title="OTA Updates">
+            <OtaUpdates />
+          </ErrorBoundary>
+        </>
+      )}
+    </>
+  );
+});
+
+const AppContainer: FC = ({}) => {
+  return (
+    <Provider store={store}>
+      <ErrorBoundary title="App">
+        <App />
+      </ErrorBoundary>
+    </Provider>
+  );
+};
+
+export default AppContainer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,83 +1,23 @@
 import { definePlugin, ServerAPI, staticClasses } from "decky-frontend-lib";
-import { FC, memo } from "react";
 import { FaShip } from "react-icons/fa";
-import TdpRange from "./components/molecules/TdpRange";
-import { TdpSlider } from "./components/molecules/TdpSlider";
-import { PollTdp } from "./components/molecules/PollTdp";
-import { store } from "./redux-modules/store";
-import { Provider } from "react-redux";
 import {
   createServerApiHelpers,
   saveServerApi,
   setValuesForGameId,
 } from "./backend/utils";
-import { TdpProfiles } from "./components/molecules/TdpProfiles";
+import { store } from "./redux-modules/store";
 import {
   acPowerEventListener,
   currentGameInfoListener,
   suspendEventListener,
 } from "./steamListeners";
 import { updateInitialLoad } from "./redux-modules/settingsSlice";
-import {
-  useFetchInitialStateEffect,
-  useIsInitiallyLoading,
-} from "./hooks/useInitialState";
+
 import { cleanupAction } from "./redux-modules/extraActions";
-import Gpu from "./components/molecules/Gpu";
-import AdvancedOptions, {
-  useIsSteamPatchEnabled,
-} from "./components/molecules/AdvancedOptions";
-import OtaUpdates from "./components/molecules/OtaUpdates";
-import ErrorBoundary from "./components/ErrorBoundary";
+
 import steamPatch from "./steamPatch/steamPatch";
-import { SteamPatchDefaultTdpSlider } from "./components/molecules/SteamPatchDefaultTdpSlider";
-import PowerControl from "./components/molecules/PowerControl";
 import { fetchPowerControlInfo } from "./redux-modules/thunks";
-import { DeckySection } from "./components/atoms/DeckyFrontendLib";
-
-const Content: FC<{ serverAPI?: ServerAPI }> = memo(({}) => {
-  useFetchInitialStateEffect();
-
-  const loading = useIsInitiallyLoading();
-
-  const steamPatchEnabled = useIsSteamPatchEnabled();
-
-  return (
-    <>
-      {!loading && (
-        <>
-          <DeckySection>
-            <TdpProfiles />
-            {!steamPatchEnabled && (
-              <>
-                <TdpSlider />
-                <Gpu />
-              </>
-            )}
-          </DeckySection>
-          <PowerControl />
-          {steamPatchEnabled && <SteamPatchDefaultTdpSlider />}
-          <TdpRange />
-          <PollTdp />
-          <AdvancedOptions />
-          <ErrorBoundary title="OTA Updates">
-            <OtaUpdates />
-          </ErrorBoundary>
-        </>
-      )}
-    </>
-  );
-});
-
-const ContentContainer: FC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
-  return (
-    <Provider store={store}>
-      <ErrorBoundary title="App">
-        <Content serverAPI={serverAPI} />
-      </ErrorBoundary>
-    </Provider>
-  );
-};
+import AppContainer from "./App";
 
 export default definePlugin((serverApi: ServerAPI) => {
   saveServerApi(serverApi);
@@ -110,7 +50,7 @@ export default definePlugin((serverApi: ServerAPI) => {
 
   return {
     title: <div className={staticClasses.Title}>SimpleDeckyTDP</div>,
-    content: <ContentContainer serverAPI={serverApi} />,
+    content: <AppContainer />,
     icon: <FaShip />,
     onDismount: () => {
       try {
