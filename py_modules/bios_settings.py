ffc3dcecc56db61808790bebbb4417e3ff78ca55
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-09-06 10:30:19 -0400

    add bios settings module

---
+++ py_modules/bios_settings.py
@@ -0,0 +1,17 @@
+import subprocess
+import json
+import decky_plugin
+import file_timeout
+
+def get_bios_settings():
+    try:
+        with file_timeout(2):
+            cmd = 'fwupdmgr get-bios-setting --json'
+            result = subprocess.run(cmd, shell=True, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            data = json.loads(result.stdout)
+            return data
+    except Exception as e:
+        decky_plugin.logger.error(f'Error get_bios_setting {e}')
+        return {}
+
