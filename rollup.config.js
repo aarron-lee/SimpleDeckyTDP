fa3339107dab294ecfa27a37a5368fdf6f7cf71f
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-09-20 00:24:46 -0400

    update dependencies and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,40 @@
-import deckyPlugin from "@decky/rollup";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import typescript from "@rollup/plugin-typescript";
+import { defineConfig } from "rollup";
+import importAssets from "rollup-plugin-import-assets";
 
-export default deckyPlugin({
-  // add extra rollup options here
+import manifest from './plugin.json' with { type: 'json' };
+
+export default defineConfig({
+  input: "./src/index.tsx",
+  plugins: [
+    commonjs(),
+    nodeResolve(),
+    typescript(),
+    json(),
+    replace({
+      preventAssignment: false,
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+    importAssets({
+      publicPath: `http://127.0.0.1:1337/plugins/${manifest.name}/`,
+    }),
+  ],
+  context: "window",
+  external: ["react", "react-dom", "@decky/ui", "@decky/manifest"],
+  output: {
+    file: "dist/index.js",
+    globals: {
+      react: "SP_REACT",
+      "react-dom": "SP_REACTDOM",
+      "@decky/ui": "DFL",
+      "@decky/manifest": JSON.stringify(manifest),
+    },
+    format: "iife",
+    exports: "default",
+  },
+  onwarn: () => undefined,
 });
