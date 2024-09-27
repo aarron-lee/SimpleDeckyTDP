871e7339b704fdc981f569e4b0633a8f8250f4a7
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-09-26 21:00:07 -0400

    add method to get cpu manufacturer

--- py_modules/device_utils.py
+++ py_modules/device_utils.py
@@ -1,6 +1,8 @@
 from enum import Enum
 import file_timeout
 import decky_plugin
+import subprocess
+import json
 
 class Devices(Enum):
   LEGION_GO = "83E1"
@@ -10,17 +12,41 @@ class Devices(Enum):
   GPD_WM2 = "G1619-04"
   GPD_WIN4 = "G1618-04"
 
-def get_device_name():
-  try:
-    with file_timeout.time_limit(2):
-      with open("/sys/devices/virtual/dmi/id/product_name", "r") as file:
-        device_name = file.read().strip()
-        file.close()
+class Vendors(Enum):
+  INTEL = "GenuineIntel"
+  AMD = "AuthenticAMD"
 
-        return device_name
-  except Exception as e:
-    decky_plugin.logger.error(f'{__name__} error while trying to read device name')
-    return ''
+CPU_VENDOR = None
+DEVICE_NAME = None
+
+def get_cpu_manufacturer():
+  global CPU_VENDOR
+
+  if not CPU_VENDOR:
+    try:
+      cmd = 'sed -En \'s/ *(( ?[^ :\t])+)\\s*(:?)/"\\1"\\3/gp\' /proc/cpuinfo'
+      result = subprocess.run(cmd, shell=True, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      json_partial = ",".join(result.stdout.split("\n"))[:-1]
+      cpu_info = json.loads(f"{{{json_partial}}}")
+      CPU_VENDOR = cpu_info.get('vendor_id')
+    except Exception as e:
+      decky_plugin.logger.error(f'{__name__} error while trying to read cpu manufacturer')
+  return CPU_VENDOR
+
+def get_device_name():
+  global DEVICE_NAME
+
+  if not DEVICE_NAME:
+    try:
+      with file_timeout.time_limit(2):
+        with open("/sys/devices/virtual/dmi/id/product_name", "r") as file:
+          device_name = file.read().strip()
+          file.close()
+
+          DEVICE_NAME = device_name
+    except Exception as e:
+      decky_plugin.logger.error(f'{__name__} error while trying to read device name')
+  return DEVICE_NAME or ''
 
 def is_rog_ally():
   device_name = get_device_name()
