5218b415ca9b6e4e1b9769eed233cb3e1cacac23
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-04-17 18:26:54 -0400

    temp disable custom

--- py_modules/devices/lenovo.py
+++ py_modules/devices/lenovo.py
@@ -2,7 +2,7 @@ import decky_plugin
 import os
 from time import sleep
 
-PLATFORM_PROFILE_PATH  = '/sys/class/platform-profile'
+# PLATFORM_PROFILE_PATH  = '/sys/class/platform-profile'
 
 LENOVO_WMI_PATH = "/sys/class/firmware-attributes/lenovo-wmi-other-0/attributes/"
 LENOVO_WMI_FAST_PATH = f"{LENOVO_WMI_PATH}/ppt_pl3_fppt/current_value"
@@ -12,7 +12,7 @@ LENOVO_WMI_STAPM_PATH = f"{LENOVO_WMI_PATH}/ppt_pl1_spl/current_value"
 # credit: https://github.com/mengmeet/PowerControl/blob/main/py_modules/devices/lenovo/lenovo_device.py
 
 def set_tdp(tdp):
-    set_custom_platform_profile()
+    # set_custom_platform_profile()
     if (
         os.path.exists(LENOVO_WMI_FAST_PATH)
         and os.path.exists(LENOVO_WMI_SLOW_PATH)
@@ -39,13 +39,14 @@ def supports_wmi_tdp():
         return True
     return False
 
-def set_custom_platform_profile():
-  if os.path.exists(PLATFORM_PROFILE_PATH):
-    decky_plugin.logger.info(f"{__name__} Setting Lenovo custom platform profile")
-    try:
-        with open(PLATFORM_PROFILE_PATH, 'w') as file:
-            file.write('custom')
-    except Exception as e:
-        decky_plugin.logger.error(f"{__name__} platform_profile error {e}")
-        sleep(1.0)
-  return True
+# custom needs some work
+# def set_custom_platform_profile():
+#   if os.path.exists(PLATFORM_PROFILE_PATH):
+#     decky_plugin.logger.info(f"{__name__} Setting Lenovo custom platform profile")
+#     try:
+#         with open(PLATFORM_PROFILE_PATH, 'w') as file:
+#             file.write('custom')
+#     except Exception as e:
+#         decky_plugin.logger.error(f"{__name__} platform_profile error {e}")
+#         sleep(1.0)
+#   return True
