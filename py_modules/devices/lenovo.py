f2cc822b01ff47a6374265a84c529d96d10b0ace
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-04-18 10:04:51 -0400

    update custom platform

--- py_modules/devices/lenovo.py
+++ py_modules/devices/lenovo.py
@@ -2,17 +2,16 @@ import decky_plugin
 import os
 from time import sleep
 
-# PLATFORM_PROFILE_PATH  = '/sys/class/platform-profile'
-
 LENOVO_WMI_PATH = "/sys/class/firmware-attributes/lenovo-wmi-other-0/attributes/"
 LENOVO_WMI_FAST_PATH = f"{LENOVO_WMI_PATH}/ppt_pl3_fppt/current_value"
 LENOVO_WMI_SLOW_PATH = f"{LENOVO_WMI_PATH}/ppt_pl2_sppt/current_value"
 LENOVO_WMI_STAPM_PATH = f"{LENOVO_WMI_PATH}/ppt_pl1_spl/current_value"
 
-# credit: https://github.com/mengmeet/PowerControl/blob/main/py_modules/devices/lenovo/lenovo_device.py
+# credit:
+# https://github.com/mengmeet/PowerControl/blob/main/py_modules/devices/lenovo/lenovo_device.py
 
 def set_tdp(tdp):
-    # set_custom_platform_profile()
+    set_platform_profile("custom")
     if (
         os.path.exists(LENOVO_WMI_FAST_PATH)
         and os.path.exists(LENOVO_WMI_SLOW_PATH)
@@ -39,14 +38,22 @@ def supports_wmi_tdp():
         return True
     return False
 
-# custom needs some work
-# def set_custom_platform_profile():
-#   if os.path.exists(PLATFORM_PROFILE_PATH):
-#     decky_plugin.logger.info(f"{__name__} Setting Lenovo custom platform profile")
-#     try:
-#         with open(PLATFORM_PROFILE_PATH, 'w') as file:
-#             file.write('custom')
-#     except Exception as e:
-#         decky_plugin.logger.error(f"{__name__} platform_profile error {e}")
-#         sleep(1.0)
-#   return True
+def set_platform_profile(profile: str) -> None:
+    name = "lenovo-wmi-gamezone"
+    base_path = find_sysdir("/sys/class/platform-profile", name)
+
+    if os.path.exists(os.path.join(base_path, "profile")):
+        try:
+            with open(os.path.join(base_path, "profile"), "w") as f:
+                f.write(profile)
+        except Exception as e:
+            decky_plugin.logger.error(f"{__name__} Failed to set platform profile {name}: {e}")
+
+def find_sysdir(prefix: str, name: str) -> str:
+    for dir in os.listdir(prefix):
+        base_path = os.path.join(prefix, dir)
+        if os.path.exists(os.path.join(base_path, "name")):
+            with open(os.path.join(base_path, "name"), "r") as f:
+                if f.read().strip() == name:
+                    return base_path
+    return None
