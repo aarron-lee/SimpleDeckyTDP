634bd2cf727eb3fa43d18bf29610ec2c9f470488
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-05-12 15:36:09 -0400

    update lenovo wmi

--- py_modules/devices/lenovo.py
+++ py_modules/devices/lenovo.py
@@ -10,6 +10,26 @@ LENOVO_WMI_STAPM_PATH = f"{LENOVO_WMI_PATH}/ppt_pl1_spl/current_value"
 # credit:
 # https://github.com/mengmeet/PowerControl/blob/main/py_modules/devices/lenovo/lenovo_device.py
 
+
+PLATFORM_PROFILE_PATH = None
+
+def find_sysdir(prefix: str, name: str) -> str:
+    for dir in os.listdir(prefix):
+        base_path = os.path.join(prefix, dir)
+        if os.path.exists(os.path.join(base_path, "name")):
+            with open(os.path.join(base_path, "name"), "r") as f:
+                if f.read().strip() == name:
+                    return base_path
+    return None
+
+def get_platform_profile_path():
+    global PLATFORM_PROFILE_PATH
+
+    if not PLATFORM_PROFILE_PATH:
+        PLATFORM_PROFILE_PATH = find_sysdir("/sys/class/platform-profile", "lenovo-wmi-gamezone")
+
+    return PLATFORM_PROFILE_PATH
+
 def set_tdp(tdp):
     set_platform_profile("custom")
     if (
@@ -34,26 +54,16 @@ def supports_wmi_tdp():
         os.path.exists(LENOVO_WMI_FAST_PATH)
         and os.path.exists(LENOVO_WMI_SLOW_PATH)
         and os.path.exists(LENOVO_WMI_STAPM_PATH)
+        and get_platform_profile_path() != None
+        and os.path.exists(get_platform_profile_path())
     ):
         return True
     return False
 
 def set_platform_profile(profile: str) -> None:
-    name = "lenovo-wmi-gamezone"
-    base_path = find_sysdir("/sys/class/platform-profile", name)
-
-    if os.path.exists(os.path.join(base_path, "profile")):
+    if os.path.exists(os.path.join(get_platform_profile_path(), "profile")):
         try:
-            with open(os.path.join(base_path, "profile"), "w") as f:
+            with open(os.path.join(get_platform_profile_path(), "profile"), "w") as f:
                 f.write(profile)
         except Exception as e:
-            decky_plugin.logger.error(f"{__name__} Failed to set platform profile {name}: {e}")
-
-def find_sysdir(prefix: str, name: str) -> str:
-    for dir in os.listdir(prefix):
-        base_path = os.path.join(prefix, dir)
-        if os.path.exists(os.path.join(base_path, "name")):
-            with open(os.path.join(base_path, "name"), "r") as f:
-                if f.read().strip() == name:
-                    return base_path
-    return None
+            decky_plugin.logger.error(f"{__name__} Failed to set platform profile {profile}: {e}")
