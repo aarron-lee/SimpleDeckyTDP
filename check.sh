64e192bb110ffd00cd8621268ea83190822e2517
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-05-19 12:45:14 -0400

    check script

---
+++ check.sh
@@ -0,0 +1,7 @@
+ #!/bin/bash
+
+sudo ryzenadj -i
+cat /sys/class/drm/card?/device/pp_od_clk_voltage
+cat /sys/devices/system/cpu/cpufreq/policy*/boost
+cat /sys/devices/system/cpu/smt/control
+cat /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
