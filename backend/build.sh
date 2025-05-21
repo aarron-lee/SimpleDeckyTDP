1e2b1a350ecf7afc4b29b36224b04ec89cc69567
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-05-20 22:43:44 -0400

    fix ryzenadj out

--- backend/build.sh
+++ backend/build.sh
@@ -5,5 +5,11 @@ cd RyzenAdj/
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
+chmod +x ryzenadj
+cp ryzenadj ../../
+cd ../..
 mkdir -p out
 cp -f ryzenadj out/
+cd out
+ls
+pwd
