7ad0ca0502e5f84eb8caa4798ba76024071e4270
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-05-23 20:51:08 -0400

    minor script update

--- check.sh
+++ check.sh
@@ -4,4 +4,5 @@ sudo ryzenadj -i
 cat /sys/class/drm/card?/device/pp_od_clk_voltage
 cat /sys/devices/system/cpu/cpufreq/policy*/boost
 cat /sys/devices/system/cpu/smt/control
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 cat /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
