498f7d1d8c6cd2ee827bf4f754f5c861307e0549
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-05-28 17:49:45 -0400

    update ryzenadj build to include license

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,4 +5,19 @@ echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
 
 cd /backend
 
-./build.sh
+echo "--- Building ryzenadj lib ---"
+git clone https://github.com/FlyGoat/RyzenAdj /tmp/ryzenadj
+cd /tmp/ryzenadj
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release .. || exit 1
+make || exit 1
+mv libryzenadj.so /backend/out/libryzenadj.so || exit 1
+mv ryzenadj /backend/out/ryzenadj || exit 1
+chmod +x /backend/out/ryzenadj || exit 1
+mv ../LICENSE /backend/out/LICENSE-ryzenadj || exit 1
+
+cd /backend
+
+echo "--- Cleaning up ---"
+# remove newly-cloned git repo and artifacts
+rm -rf ./ryzenadj
