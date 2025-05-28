b172b600eaffd9f7c25e17fe75575327017f2585
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-05-28 18:07:28 -0400

    remove ryzenadj submodule

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# credit: https://github.com/totallynotbakadestroyer/Decky-Undervolt/blob/master/backend/entrypoint.sh
+
 set -e
 
 echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
