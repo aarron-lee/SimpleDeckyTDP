d296009bea87dd9fe4c3cd547f027ad683c65332
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-08-08 07:16:32 -0400

    fix install script

--- install.sh
+++ install.sh
@@ -6,25 +6,38 @@ if [ "$EUID" -eq 0 ]
   exit
 fi
 
+PLUGIN_NAME=SimpleDeckyTDP
+PLUGIN_DIR=$HOME/homebrew/plugins/$PLUGIN_NAME
 
 echo "removing previous install if it exists"
 
 cd $HOME
 
-sudo rm -rf $HOME/homebrew/plugins/SimpleDeckyTDP
+sudo rm -rf $PLUGIN_DIR
 
-echo "installing SimpleDeckyTDP plugin for TDP control"
+echo "installing $PLUGIN_NAME plugin for TDP control"
 # download + install simple decky tdp
-curl -L $(curl -s https://api.github.com/repos/aarron-lee/SimpleDeckyTDP/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/SimpleDeckyTDP.zip
-sudo 7z x ./SimpleDeckyTDP.zip  -o$HOME/homebrew/plugins
+curl -L $(curl -s https://api.github.com/repos/aarron-lee/$PLUGIN_NAME/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/$PLUGIN_NAME.zip
 
-# curl -L $(curl -s https://api.github.com/repos/aarron-lee/SimpleDeckyTDP/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/SimpleDeckyTDP.tar.gz
-# sudo tar -xzf SimpleDeckyTDP.tar.gz -C $HOME/homebrew/plugins
+chmod -R +w "$HOME/homebrew/plugins"
+mkdir -p $PLUGIN_DIR
 
-# sudo ark -ba ./SimpleDeckyTDP.zip -o $HOME/homebrew/plugins
+TMP_DIR=/tmp/$PLUGIN_NAME
+
+7z x ./$PLUGIN_NAME.zip  -o/tmp
+
+sudo rsync -av $TMP_DIR/* $PLUGIN_DIR --delete
+
+sudo chown root $PLUGIN_DIR/plugin.json
+
+# curl -L $(curl -s https://api.github.com/repos/aarron-lee/$PLUGIN_NAME/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/$PLUGIN_NAME.tar.gz
+# sudo tar -xzf $PLUGIN_NAME.tar.gz -C $HOME/homebrew/plugins
+
+# sudo ark -ba ./$PLUGIN_NAME.zip -o $HOME/homebrew/plugins
 
 # install complete, remove build dir
-rm  $HOME/SimpleDeckyTDP.zip
+rm  $HOME/$PLUGIN_NAME.zip
+# rm -rf $TMP_DIR
 sudo systemctl restart plugin_loader.service
 
 echo "Installation complete"
