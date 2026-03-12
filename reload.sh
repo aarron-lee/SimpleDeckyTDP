e616be95409cd96a67af56d87ee9da004a33ae08
Aarron Lee <aarron-lee@users.noreply.github.com>
2026-03-12 09:28:26 -0400

    dev config update for i18n

--- reload.sh
+++ reload.sh
@@ -1,6 +1,12 @@
  #!/bin/bash
 
 ./scripts/build_i18n_json.sh
+if [ -L "./i18n" ]; then
+  echo "The symlink ./i18n exists."
+else
+  # echo "The symlink ./i18n does not exist."
+  ln -s ./defaults/i18n ./i18n
+fi
 pnpm run build
 sudo rm -r /home/$USER/homebrew/plugins/SimpleDeckyTDP/
 sudo cp -r /home/$USER/Development/SimpleDeckyTDP/ ~/homebrew/plugins/
