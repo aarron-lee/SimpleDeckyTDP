91f686fa864d2931af3594f14fe185fd81026c59
Aarron Lee <aarron-lee@users.noreply.github.com>
2026-05-10 13:08:55 -0400

    update install.sh to enable specific versions

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 # does the following:
 # - SimpleDeckyTDP Decky Plugin
+
+VERSION=${VERSION:-"LATEST"}
+
+
 if [ "$EUID" -eq 0 ]
   then echo "Please do not run as root"
   exit
@@ -14,8 +18,15 @@ cd $HOME
 sudo rm -rf $HOME/homebrew/plugins/SimpleDeckyTDP
 
 echo "installing SimpleDeckyTDP plugin for TDP control"
+
+
+FINAL_URL='https://api.github.com/repos/aarron-lee/SimpleDeckyTDP/releases/latest'
+if [ $VERSION != "LATEST" ] ; then
+  FINAL_URL="https://api.github.com/repos/aarron-lee/SimpleDeckyTDP/releases/tags/$VERSION"
+fi
+
 # download + install simple decky tdp
-curl -L $(curl -s https://api.github.com/repos/aarron-lee/SimpleDeckyTDP/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/SimpleDeckyTDP.zip
+curl -L $(curl -s $FINAL_URL | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/SimpleDeckyTDP.zip
 sudo 7z x ./SimpleDeckyTDP.zip  -o$HOME/homebrew/plugins
 
 # curl -L $(curl -s https://api.github.com/repos/aarron-lee/SimpleDeckyTDP/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/SimpleDeckyTDP.tar.gz
