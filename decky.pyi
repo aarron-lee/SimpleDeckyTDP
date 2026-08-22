a1229d0e4d1620606c36d18bbcdb2b6b69cffef2
Aarron Lee <aarron-lee@users.noreply.github.com>
2026-08-22 14:27:37 -0400

    Release version 1.0.6

--- decky.pyi
+++ decky.pyi
@@ -12,7 +12,7 @@ Some basic migration helpers are available: `migrate_any`, `migrate_settings`, `
 A logging facility `logger` is available which writes to the recommended location.
 """
 
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 
 import logging
 
